9a2f98572765e390fd7d7939780bc2f259fd1b4b
appuser <appuser@journey2-95cdcd64d-4xpqt>
2023-03-29 09:40:28 +0000

    starter project

---
+++ client/public/script.js
@@ -0,0 +1,14 @@
+const meButton = `<button id="me">Click me!</button>`;
+
+const greetings = [`Hi`, `Hello`, `Ciao`, `Arrivederci`];
+
+const loadEvent = function() {
+
+  // Write your JavaScript code after this line
+
+  
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
